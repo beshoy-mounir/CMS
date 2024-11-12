577aea1229c2a08ea9439f798c5f24fb5813e7a5
Beshoy <beshoy_mounir22@yahoo.com>
2024-11-12 14:51:59 +0200

    removed line from last commit

--- src/Pages/UserLayout/Home/Home.jsx
+++ src/Pages/UserLayout/Home/Home.jsx
@@ -1,5 +1,4 @@
 const Home = () => {
-    ``;
     return (
         <div>
             <div className="flex justify-center items-center gap-4 w-full h-lvh text-2xl font-black text-white bg-primaly dark:bg-gray-800">
